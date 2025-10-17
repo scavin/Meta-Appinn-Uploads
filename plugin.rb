5b3de42229ed3e077b0fe3ac77aac5f002028174
scavin <scavina@gmail.com>
2025-10-18 00:08:37 +0800

    chore: update plugin authorship

--- plugin.rb
+++ plugin.rb
@@ -3,5 +3,5 @@
 # name: meta-appinn-uploads
 # about: Meta Appinn Uploads replaces Discourse attachments with uploads to h1.appinn.me and Markdown links.
 # version: 0.1.0
-# authors: xymoryn, Appinn Community
+# authors: Appinn Community
 # url: https://github.com/scavin/Meta-Appinn-Uploads
