60f13b463e16a404d04bb4d66594a099daca3c04
scavin <scavina@gmail.com>
2025-10-17 23:36:19 +0800

    chore: point plugin url to scavin repo

--- plugin.rb
+++ plugin.rb
@@ -4,4 +4,4 @@
 # about: Meta Appinn Uploads replaces Discourse attachments with uploads to h1.appinn.me and Markdown links.
 # version: 0.1.0
 # authors: xymoryn, Appinn Community
-# url: https://github.com/xymoryn/user-scripts
+# url: https://github.com/scavin/Meta-Appinn-Uploads
